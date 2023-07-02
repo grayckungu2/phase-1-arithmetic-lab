9500c4d770ae84c9df7c193c40f7bb221f05150f
Grace kungu <grayckungu2@gmail.com>
2023-07-02 20:10:41 +0300

    updated changes

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function random() {
+  return Math.floor(Math.random() * 100) + 1;
+}
+
+function mod(num3, num4) {
+  return num3 % num4;
+}
+
+function max(numbers) {
+  return Math.max(...numbers);
+}
